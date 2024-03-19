cd22fdc55785461f8dda4d7e23d3dfc73ba327c5
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-03-19 18:53:16 -0400

    updated paragraph

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -6,16 +6,15 @@ function About() {
     <div className="about" id="about">
       <div id="lastName" className="about__text">
         <div>NAMASTE.</div>
-        <div>I &nbsp;&nbsp;&nbsp;AM </div>
+        <div>I &nbsp;AM </div>
         GURUNG,
         <span>&nbsp;&nbsp;TSHERING</span>
       </div>
       {/* <div className="about__text__name" anchor="lastName">TSHERING</div> */}
       <div className="about__text__body">
         I am from Nepal. I have
-        worked in hotel and retail sector. I have always wanted to create
-        something on the web but I never had oppurtunity until I joined Pursuit,
-        a codeing bootcam where I learned full stack web development. I aim to challange myself and turn my ideas and dreams into reality.
+        worked in hotel and retail sector. I joined Pursuit,
+        a codeing bootcamp where I learned full stack web development. I aim to challange myself and turn my ideas and my dreams into reality.
         <br/> When I am not infront of a computer, either I partake in pretend play with my son or whip something delicious in the kitchen.
       </div>
     </div>
